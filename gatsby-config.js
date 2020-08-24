222555ee21721d8b3812cf4e4cd984c67efefe1e
Kevin Mees <kev.mees@gmail.com>
2020-08-24 16:27:18 +0200

    remove markdown stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,30 +30,30 @@ module.exports = {
         }
       }
     },
-    {
-      resolve: 'gatsby-transformer-remark',
-      options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: {
-              wrapperStyle: 'margin-bottom: 1rem'
-            }
-          },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              maxWidth: 1140,
-              quality: 90,
-              linkImagesToOriginal: false
-            }
-          }
-        ]
-      }
-    },
+    // {
+    //   resolve: 'gatsby-transformer-remark',
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: 'gatsby-remark-responsive-iframe',
+    //         options: {
+    //           wrapperStyle: 'margin-bottom: 1rem'
+    //         }
+    //       },
+    //       'gatsby-remark-prismjs',
+    //       'gatsby-remark-copy-linked-files',
+    //       'gatsby-remark-smartypants',
+    //       {
+    //         resolve: 'gatsby-remark-images',
+    //         options: {
+    //           maxWidth: 1140,
+    //           quality: 90,
+    //           linkImagesToOriginal: false
+    //         }
+    //       }
+    //     ]
+    //   }
+    // },
     'gatsby-transformer-json',
     {
       resolve: 'gatsby-plugin-canonical-urls',
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,4 @@
-'use strict'
-
-const path = require('path')
+// const path = require('path')
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
@@ -11,75 +9,75 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
   // trip up. An empty string is still required in replacement to `null`.
 
   switch (node.internal.type) {
-    case 'MarkdownRemark': {
-      const { permalink, layout } = node.frontmatter
-      const { relativePath } = getNode(node.parent)
+    case 'MarkdownRemark':
+      {
+        const { permalink, layout } = node.frontmatter
+        const { relativePath } = getNode(node.parent)
 
-      let slug = permalink
+        let slug = permalink
 
-      if (!slug) {
-        slug = `/${relativePath.replace('.md', '')}/`
+        if (!slug) {
+          slug = `/${relativePath.replace('.md', '')}/`
+        }
+
+        // Used to generate URL to view this content.
+        createNodeField({
+          node,
+          name: 'slug',
+          value: slug || ''
+        })
+
+        // Used to determine a page layout.
+        createNodeField({
+          node,
+          name: 'layout',
+          value: layout || ''
+        })
       }
-
-      // Used to generate URL to view this content.
-      createNodeField({
-        node,
-        name: 'slug',
-        value: slug || ''
-      })
-
-      // Used to determine a page layout.
-      createNodeField({
-        node,
-        name: 'layout',
-        value: layout || ''
-      })
-    }
+      break
+    default:
+      break
   }
 }
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const allMarkdown = await graphql(`
-    {
-      allMarkdownRemark(limit: 1000) {
-        edges {
-          node {
-            fields {
-              layout
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  if (allMarkdown.errors) {
-    console.error(allMarkdown.errors)
-    throw new Error(allMarkdown.errors)
-  }
-
-  allMarkdown.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    const { slug, layout } = node.fields
-
-    createPage({
-      path: slug,
-      // This will automatically resolve the template to a corresponding
-      // `layout` frontmatter in the Markdown.
-      //
-      // Feel free to set any `layout` as you'd like in the frontmatter, as
-      // long as the corresponding template file exists in src/templates.
-      // If no template is set, it will fall back to the default `page`
-      // template.
-      //
-      // Note that the template has to exist first, or else the build will fail.
-      component: path.resolve(`./src/templates/${layout || 'page'}.tsx`),
-      context: {
-        // Data passed to context is available in page queries as GraphQL variables.
-        slug
-      }
-    })
-  })
+exports.createPages = async (/* { graphql, actions } */) => {
+  // const { createPage } = actions
+  // const allMarkdown = await graphql(`
+  //   {
+  //     allMarkdownRemark(limit: 1000) {
+  //       edges {
+  //         node {
+  //           fields {
+  //             layout
+  //             slug
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  // if (allMarkdown.errors) {
+  //   console.error(allMarkdown.errors)
+  //   throw new Error(allMarkdown.errors)
+  // }
+  // allMarkdown.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  //   const { slug, layout } = node.fields
+  //   createPage({
+  //     path: slug,
+  //     // This will automatically resolve the template to a corresponding
+  //     // `layout` frontmatter in the Markdown.
+  //     //
+  //     // Feel free to set any `layout` as you'd like in the frontmatter, as
+  //     // long as the corresponding template file exists in src/templates.
+  //     // If no template is set, it will fall back to the default `page`
+  //     // template.
+  //     //
+  //     // Note that the template has to exist first, or else the build will fail.
+  //     component: path.resolve(`./src/templates/${layout || 'page'}.tsx`),
+  //     context: {
+  //       // Data passed to context is available in page queries as GraphQL variables.
+  //       slug
+  //     }
+  //   })
+  // })
 }
